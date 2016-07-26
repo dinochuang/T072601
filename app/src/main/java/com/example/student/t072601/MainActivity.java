0b9332a8887afd8c8251bb9baa9212bb94869f65
dino <dino6535@hotmail.com>
2016-07-26 11:55:29 +0800

    Layout 2

--- app/src/main/java/com/example/student/t072601/MainActivity.java
+++ app/src/main/java/com/example/student/t072601/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.student.t072601;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       // setContentView(R.layout.activity_main);
+
+        Button btn = new Button(MainActivity.this);
+        btn.setText("Click Me!");
+        setContentView(btn);
     }
 }
